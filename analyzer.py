573de11e059c0769381f578374e48473a626d483
23tae <taehoonkim.dev@gmail.com>
2023-12-11 18:45:24 +0900

    Refactor: 이미지 분석 함수 리팩토링

---
+++ analyzer.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import detect
+
+
+def analyze_images(profiles, image_root_dir, methods):
+    print('Analyzing images...\n')
+    output_root_dir = os.path.join(os.getcwd(), 'result')
+    for category, names in profiles.items():  # per category
+        print('category:', category)
+        for name in names:  # per user
+            print('user:', name)
+            for i in range(50):  # per image
+                if os.path.exists(os.path.join(image_root_dir, category, name, str(i) + '.jpg')):
+                    for method in methods:
+                        sys.stdout = open(os.path.join(
+                            output_root_dir, category, method, name, str(i) + '.txt'), 'w')
+                        if method == 'labels':
+                            detect.detect_labels(os.path.join(
+                                image_root_dir, category, name, str(i) + '.jpg'))
+                        elif method == 'properties':
+                            detect.detect_properties(os.path.join(
+                                image_root_dir, category, name, str(i) + '.jpg'))
+                        elif method == 'object-localization':
+                            detect.localize_objects(os.path.join(
+                                image_root_dir, category, name, str(i) + '.jpg'))
+                        else:
+                            print('invalid method')
+                        sys.stdout = sys.__stdout__
+
+                else:  # no image
+                    break
+    print('Done.\n')
--- main.py
+++ main.py
@@ -1,49 +1,17 @@
-import sys
-import os
+import time
 import input_data
 import directory
-import detect
-import time
+import analyzer
 
 image_root_dir = '../instagram-image-downloader/result/'
 methods = ['labels', 'properties', 'object-localization']
-
-
-def analyze_images(profiles):
-    print('Analyzing images...\n')
-    output_root_dir = os.path.join(os.getcwd(), 'result')
-    for category, names in profiles.items():  # per category
-        print('category:', category)
-        for name in names:  # per user
-            print('user:', name)
-            for i in range(50):  # per image
-                if os.path.exists(os.path.join(image_root_dir, category, name, str(i) + '.jpg')):
-                    for method in methods:
-                        sys.stdout = open(os.path.join(
-                            output_root_dir, category, method, name, str(i) + '.txt'), 'w')
-                        if method == 'labels':
-                            detect.detect_labels(os.path.join(
-                                image_root_dir, category, name, str(i) + '.jpg'))
-                        elif method == 'properties':
-                            detect.detect_properties(os.path.join(
-                                image_root_dir, category, name, str(i) + '.jpg'))
-                        elif method == 'object-localization':
-                            detect.localize_objects(os.path.join(
-                                image_root_dir, category, name, str(i) + '.jpg'))
-                        else:
-                            print('invalid method')
-                        sys.stdout = sys.__stdout__
-
-                else:  # no image
-                    break
-    print('Done.\n')
 
 
 def main():
     start = time.time()
     profiles = input_data.get_profiles(image_root_dir)
     directory.make_output_directory(profiles, methods)
-    analyze_images(profiles)
+    analyzer.analyze_images(profiles, image_root_dir, methods)
     elapsed_time = time.time() - start
     elapsed_time_rounded = round(elapsed_time, 2)
     print("Total time:", str(elapsed_time_rounded) + 's')
